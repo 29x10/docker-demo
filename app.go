9b39edacfcb1b3ecfdc79f59772194a57339f8f9
binlei <kongpo0412@gmail.com>
2019-01-04 00:49:53 +0800

    update version to 2

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ func main() {
     app := iris.Default()
     app.Get("/", func(ctx iris.Context) {
         ctx.JSON(iris.Map{
-            "version": 1,
+            "version": 2,
         })
     })
     app.Run(iris.Addr(":8080"))
